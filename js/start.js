e7e2379d26add6e9067911eb634168a14b5b7b1a
developerpark2348 <developerpark2348@gmail.com>
2021-02-27 22:43:01 +0900

    Make qna and style
    css gradient 만들시 유용한 사이트
    https://www.colorzilla.com/

--- js/start.js
+++ js/start.js
@@ -1,5 +1,44 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 12;
+
+function addAnswer(answerText, qIdx){
+    var a = document.querySelector('.answerBox'); //변수a에 .answerBox를 담음.
+    var answer = document.createElement('button'); //button을 만드는 변수 answer
+    answer.classList.add('answerList'); //answer에 'answerList'클래스를 부여함.
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+
+    a.appendChild(answer); //변수a 즉, .answerBox라는 div태그내에 자식으로 answer(button을 만드는 변수)를 넣는다.
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll(".answerList");
+        for(let i = 0; i < children.length; i++){
+            children[i].disabled = true;
+            children[i].style.WebkitAnimation = 'fadeOut 0.5s';
+            children[i].style.animation = 'fadeOut 0.5s';
+        }
+        setTimeout(()=>{
+            for(let i = 0; i < children.length; i++){
+                children[i].style.display = 'none';
+            }
+            goNext(++qIdx);
+        }, 450)
+    }, false);
+}
+
+function goNext(qIdx) {
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a){ //i라는 변수를 만든후 qnaList[qIdx].a의 수만큼 반복하는 반복문을 뜻함.
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx + 1) + '%';
+}
 
 function  begin() {
     main.style.WebkitAnimation = "fadeOut 1s";
@@ -15,6 +54,8 @@ function  begin() {
             main.style.display = "none";
             qna.style.display = "block";
         }, 450);
+        let qIdx = 0;
+        goNext(qIdx);
     }, 450);
 }
 // => main.style.animation에 fadeOut을 1초동안 애니메이션을 진행하며,
